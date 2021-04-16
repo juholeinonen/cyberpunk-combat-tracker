a61c0ac30eb2d371d04965cc7121fee868a656ea
Juho <juho.leinonen@aalto.fi>
2021-04-16 11:26:06 +0300

    start of armor class

---
+++ armor.py
@@ -0,0 +1,9 @@
+
+
+class Armor:
+    def __init__(self, name, hardness, sp, edged_weakness, health):
+        self.name = name
+        self.hardness = hardness
+        self.sp = sp
+        self.edged_weakness = edged_weakness
+        self.health = health
